f3748e5223299590c27689441b7469798ad2b001
white-hun <121800818+white-hun@users.noreply.github.com>
2023-02-14 16:39:13 +0900

    Update CSS

--- practice-mini-blog/src/component/ui/TextInput.jsx
+++ practice-mini-blog/src/component/ui/TextInput.jsx
@@ -11,6 +11,15 @@ const StyledTextarea = styled.textarea`
   padding: 16px;
   font-size: 16px;
   line-height: 20px;
+  opacity: 0.9;
+  :focus {
+    border: 1px solid #00c6f7;
+    border-radius: 15px;
+    outline: 2px solid #00c6f7;
+    transition: 0.5s;
+  }
+  transition: 0.5s;
+  border: 1px solid grey;
 `;
 
 function TextInput(props) {
