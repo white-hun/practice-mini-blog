7b8a0b198fdd1c9de2201cebf9f81d398d15a055
white-hun <121800818+white-hun@users.noreply.github.com>
2023-02-14 15:05:55 +0900

    Update CommentListItem.jsx

--- practice-mini-blog/src/component/list/CommentListItem.jsx
+++ practice-mini-blog/src/component/list/CommentListItem.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 const Wrapper = styled.div`
   width: calc(100% - 32px);
-  padding: 16px;
+  padding: 8px 16px;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
